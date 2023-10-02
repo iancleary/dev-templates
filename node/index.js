0ee33c376b8065ba108ce0e8461c199a9f8d5120
iancleary <github@iancleary.me>
2023-10-02 16:32:14 -0700

    Remove index.js

--- node/index.js
+++
@@ -1,7 +0,0 @@
-import chalk from "chalk";
-
-const message = `${chalk.blue(
-  "Hello from inside your Nix-flake-provided"
-)} ${chalk.green("Node.js")} ${chalk.blue("development environment")}`;
-
-console.log(message);
